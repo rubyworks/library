7225550df37db04c3196b0edf493af2c6a03b8ae
7rans <transfire@gmail.com>
2012-01-08 07:03:40 -0500

    Better message support in LoadError.

--- lib/library/load_error.rb
+++ lib/library/load_error.rb
@@ -9,8 +9,16 @@ class Library
     #
     def initialize(failed_path, library_name=nil)
       super()
+
       @failed_path  = failed_path
       @library_name = library_name
+
+      if library_name
+        @message = "#{@library_name}:#{@failed_path}"
+      else
+        @message = failed_path
+      end
+
       clean_backtrace
     end
 
@@ -18,19 +26,17 @@ class Library
     # Error message string.
     #
     def to_s
-      "LoadError: cannot load such file -- #{@library_name}:#{@failed_path}"
+      "LoadError: cannot load such file -- #{@message}"
     end
 
     #
     # Take an +error+ and remove any mention of 'library' from it's backtrace.
-    # Will leave the backtrace untouched if $DEBUG is set to true.
-    #
-    # @todo Make clean_backtrace this more robust.
+    # Will leaving the backtrace untouched if $DEBUG is set to true.
     #
     def clean_backtrace
       return if ENV['debug'] || $DEBUG
       bt = backtrace
-      bt = bt.reject{ |e| /library/ =~ e } if bt
+      bt = bt.reject{ |e| $RUBY_IGNORE_CALLERS.any?{ |re| re =~ e } } if bt
       set_backtrace(bt)
     end
   end
