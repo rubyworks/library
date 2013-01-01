080f0cef1f6a29eb591351627216866f49b60aa9
7rans <transfire@gmail.com>
2013-01-01 10:10:39 -0500

    Split core extensions up by class.

--- lib/library/core_ext.rb
+++ lib/library/core_ext.rb
@@ -1,34 +1,3 @@
-class File
-
-  #
-  RE_PATH_SEPERATOR = Regexp.new('[' + Regexp.escape(File::Separator) + %q{\\\/} + ']')
-
-  #
-  def self.split_root(path)
-    path.split(RE_PATH_SEPERATOR, 2)
-  end
-
-end
-
-class Hash
-
-  #
-  # Transform keys of hash returning a new hash.
-  #
-  def rekey #:yield:
-    if block_given?
-      inject({}){|h,(k,v)| h[yield(k)]=v; h}
-    else
-      inject({}){|h,(k,v)| h[k.to_sym]=v; h}
-    end
-  end
-
-  #
-  # In-place rekey.
-  #
-  def rekey! #:yield:
-    replace(rekey{|k| yield(k) })
-  end
-
-end
+require_relative 'core_ext/file'
+require_relative 'core_ext/hash'
 
