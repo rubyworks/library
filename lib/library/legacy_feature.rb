d0ba30b0feaa4764e86911f364c4adcc53cc3b6b
7rans <transfire@gmail.com>
2013-01-03 12:54:52 -0500

    Add LegacyFeature to handle features not found in the ledger.

---
+++ lib/library/legacy_feature.rb
@@ -0,0 +1,46 @@
+class Library
+
+  class LegacyFeature < Feature
+
+    #
+    def initialize(pathname)
+      @library   = nil
+      @fullname  = pathname
+      @loadpath  = File.dirname(pathname)    # not that this makes any real sense, but...
+      @filename  = File.basename(pathname)
+      @extension = nil
+
+      @required = {}
+    end
+
+=begin
+    #
+    def load(options={})
+      Library.load_stack << self
+      begin
+        @required = true if options[:require]
+        require_without_library(fullname)
+      ensure
+        Library.load_stack.pop
+      end
+    end
+
+    #
+    def require(options={})
+      Library.load_stack << self
+      begin
+        load_without_library(fullname, options[:wrap])
+      ensure
+        Library.load_stack.pop
+      end
+    end
+
+    #
+    def required?
+      @required
+    end
+=end
+
+  end
+
+end
